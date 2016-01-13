8caedf1425320a78c28bba6f94a48d31136626d7
Eli <elimelec.burghelea@gmail.com>
2016-01-14 00:55:10 +0200

    Print program

--- tursib.rb
+++ tursib.rb
@@ -37,10 +37,57 @@ def print_route route
 	print_stops returning
 end
 
+def find_stops route, stop
+	route = "http://tursib.ro/en/traseu/#{route}"
+	route = Nokogiri::HTML(open(route))
+
+	going = route.css("div.fl table.statii tr")
+	returning = route.css("div.fr table.statii tr")
+
+	links = []
+	(going + returning).each do |s|
+		s = s.css("td a").first
+		next unless s
+
+		name = s.text
+		link = s.attribute("href").value
+
+		links.push link if name[/#{stop}/i]
+	end
+
+	links.map do |link|
+		"http://tursib.ro#{link}"
+	end
+end
+
+def print_program route, stop
+	links = find_stops route,stop
+	links.each do |link|
+		stop = Nokogiri::HTML(open(link))
+		name = stop.css("h2 span").first
+		name = name.text + name.next.text
+		p name
+		p
+
+		hours = stop.css("div.plecari")
+
+		hours.each do |h|
+			h =  h.css("div")
+			h.each do |h|
+				print h.text + "\t"
+			end
+			puts
+			puts
+		end
+	end
+end
 
 route = ARGV[0]
+stop = ARGV[1]
 
-if route
+if stop
+	print_program route, stop
+elsif route
 	print_route route
 else
 	print_routes
